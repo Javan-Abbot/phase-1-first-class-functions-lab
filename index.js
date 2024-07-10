67aeacf0b72f432df1a1598ebe1eceb6af96e07c
+javan abbot <+>
2024-07-10 12:22:53 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+// Code your solution in this file!
+
+const array=['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers=function(array){
+    return (array.slice(0,2));
+}
+const returnLastTwoDrivers=function(array){
+    return (array.slice(2));
+}
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+  
+const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+      return fare * multiplier;
+    };
+  };
+const fareDoubler=createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3);
+function selectDifferentDrivers(array,fun){
+  if (fun===returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(array);
+  }
+  else{
+    return returnLastTwoDrivers(array)
+  }
+}
